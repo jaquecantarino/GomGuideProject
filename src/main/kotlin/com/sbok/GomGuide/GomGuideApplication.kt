58b037c5e9a69fa89e66136c51fd0c91877aecf1
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2025-04-26 02:21:13 -0300

    Iniciando

---
+++ src/main/kotlin/com/sbok/GomGuide/GomGuideApplication.kt
@@ -0,0 +1,11 @@
+package com.sbok.GomGuide
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GomGuideApplication
+
+fun main(args: Array<String>) {
+	runApplication<GomGuideApplication>(*args)
+}
